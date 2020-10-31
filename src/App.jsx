fe0cbd56802f1dc362f8ebffd1784f70a17091c4
Harsh Vishwakarma <harsh03121997@gmail.com>
2020-10-31 11:09:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./components/Header"
+import Foooter from "./components/Footer"
+import Notes from "./components/Notes"
+function App(){
+    return(
+        <div>
+            <Header ></Header>
+            <Notes />
+            <Foooter />
+        </div>
+        
+        
+    )
+}
+export default App;
